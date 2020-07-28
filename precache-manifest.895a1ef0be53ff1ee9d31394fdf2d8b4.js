ecf8f73c162929d8cca47357a1560393489798b0
mather <mather@users.noreply.github.com>
2020-07-28 03:45:01 +0000

    deploy: 6e2feada027c2bc6d6f7c28b6d6535a9fb243ed4

---
+++ precache-manifest.895a1ef0be53ff1ee9d31394fdf2d8b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/simple-stopwatch/favicon.ico"
+  },
+  {
+    "revision": "932cbd1bae7bcc7182c2987f11bc71a6",
+    "url": "/simple-stopwatch/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/simple-stopwatch/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/simple-stopwatch/manifest.json"
+  },
+  {
+    "revision": "24beb463aaa9c203f131",
+    "url": "/simple-stopwatch/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "24beb463aaa9c203f131",
+    "url": "/simple-stopwatch/static/js/main.24beb463.chunk.js"
+  },
+  {
+    "revision": "b6fcf50a44302e727ae7",
+    "url": "/simple-stopwatch/static/js/runtime~main.b6fcf50a.js"
+  },
+  {
+    "revision": "b9063112a1dd4b93007d",
+    "url": "/simple-stopwatch/static/js/vendors~main.b9063112.chunk.js"
+  }
+]);
